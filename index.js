217d420f3b4d2475e530877fa7b72ea27c5090b0
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-02-19 12:39:39 +0300

    Json objects

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var isBirthdayData = true;
 var ageData = 40;
 var userNameData = 'John';
+var salary;
+salary = 5000;
+var userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}';
+var userObj = JSON.parse(userData);
 function logBrtMsg(isBirthday, userName, age) {
     if (isBirthday) {
         console.log("Congrats ".concat(userName, ", age: ").concat(age + 1));
     }
 }
-function logBrtMsg2(isBirthday, userName, age) {
+var logBrtMsg2 = function (isBirthday, userName, age) {
     if (isBirthday) {
         return "Congrats ".concat(userName, ", age: ").concat(age + 1);
     }
     else {
         return 'Error';
     }
-}
+};
 logBrtMsg(isBirthdayData, userNameData, ageData);
 logBrtMsg2(isBirthdayData, userNameData, ageData);
--- index.ts
+++ index.ts
@@ -2,6 +2,16 @@ const isBirthdayData: boolean = true;
 const ageData: number = 40;
 const userNameData: string = 'John';
 
+let salary: number;
+salary = 5000;
+
+const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}';
+const userObj: {
+    isBirthdayData: boolean,
+    userNameData: string, 
+    ageData: number
+} = JSON.parse(userData);
+
 function logBrtMsg (isBirthday: boolean, userName: string, age: number): void {
     if (isBirthday) {
         console.log(`Congrats ${userName}, age: ${age + 1}`)
