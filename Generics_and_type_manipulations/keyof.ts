1b26cbf9267f6aeae1900ca74ed5f573cc1e7922
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-03-01 18:10:24 +0300

    keyof

---
+++ Generics_and_type_manipulations/keyof.ts
@@ -0,0 +1,26 @@
+interface ICompany {
+    name: string,
+    debts: number,
+}
+
+type CompanyKeys = keyof ICompany;
+const keys: CompanyKeys = 'name';
+
+function printDebts<T, K extends keyof T, S extends keyof T>(company: T, name: K, debts: S) {
+    console.log(`Company ${company[name]}`);
+    console.log(`Debts ${company[debts]}`);
+}
+
+const hh:ICompany = {
+    name: 'HH',
+    debts: 500000
+}
+
+printDebts(hh, 'name', 'debts');
+
+const google = {
+    name: 'Google',
+    open: 'true'
+}
+
+printDebts(google, 'name', 'open');
